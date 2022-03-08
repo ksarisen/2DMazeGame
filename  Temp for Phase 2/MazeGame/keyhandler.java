9df5dfa229c40a5a9f2a75f7c84d2cdb36df9e6e
hma70 <hma70@sfu.ca>
2022-03-08 02:48:03 +0000

    Upload New File

---
+++  Temp for Phase 2/MazeGame/keyhandler.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class keyhandler implements KeyListener {
+
+    public boolean uppressed,downpressed,leftpressed,rightpressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            uppressed = true;
+        }
+        if(code == KeyEvent.VK_S){
+            downpressed = true;
+        }
+        if(code == KeyEvent.VK_A){
+            leftpressed = true;
+        }
+        if(code == KeyEvent.VK_D){
+            rightpressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            uppressed = false;
+        }
+        if(code == KeyEvent.VK_S){
+            downpressed = false;
+        }
+        if(code == KeyEvent.VK_A){
+            leftpressed = false;
+        }
+        if(code == KeyEvent.VK_D){
+            rightpressed = false;
+        }
+    }
+    }
+
