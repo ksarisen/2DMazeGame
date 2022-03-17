8f7c48ceeff9dbeb7373b7def7045dace22f39a4
Reece Landry <reece_landry@sfu.ca>
2022-03-17 21:33:50 +0000

    Added Helicopter.java

---
+++  Temp for Phase 2/src/main/java/Objects/Helicopter.java
@@ -0,0 +1,20 @@
+package objects;
+import MazeGame.GameObject;
+import Textures.Image;
+
+/**
+ * Creates object with grass image
+ * @author Reece Landry
+ *	
+ */
+
+public class Helicopter extends GameObject {
+
+	public Helicopter(String x) {
+		super.texture = new Image("Textures/Helicopter.png");
+	}
+	public Helicopter() {
+		super.texture = new Image("Textures/Helicopter.png");
+	}
+	
+}
