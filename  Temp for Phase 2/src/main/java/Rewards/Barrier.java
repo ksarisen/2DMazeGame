c944c0d813fdf7c49e876f06cfffe1ebf9fdf206
maintainer <yuwenj@sfu.ca>
2022-03-17 16:50:54 -0700

    Change Barrier to Rewards

---
+++  Temp for Phase 2/src/main/java/Rewards/Barrier.java
@@ -0,0 +1,16 @@
+package main.java.Rewards;
+import main.java.Textures.Image;
+
+import java.awt.*;
+
+// Randomly appears!!
+public class Barrier extends Reward {
+    public Barrier(int value, Image texture, Point location) {
+        super(value, texture, location);
+    }
+
+    @Override
+    public Point getLocation() {
+        return super.getLocation();
+    }
+}
