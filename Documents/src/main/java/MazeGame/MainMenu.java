1191f3a2202d74839e4e03e769dec384822968b8
maintainer <yuwenj@sfu.ca>
2022-03-19 21:05:03 -0700

    Add the 3 menus of this game.

---
+++ Documents/src/main/java/MazeGame/MainMenu.java
@@ -0,0 +1,70 @@
+package MazeGame;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MainMenu {
+    static JFrame jf = new JFrame("Start Menu");
+    public static void firtPage() {
+
+        jf.setPreferredSize(new Dimension(700, 700));
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jf.setLayout(null);
+        JButton jl = new JButton("START");
+        jl.setPreferredSize(new Dimension(680, 30));
+        jf.add(jl);
+        jl.setBounds(0, 200, 690, 30);
+        jl.setFont(new Font("", Font.BOLD, 24));
+        jl.setForeground(Color.decode("#375a7f"));
+
+
+        JButton jl1 = new JButton("EXIT");
+        jl1.setPreferredSize(new Dimension(680, 30));
+        jf.add(jl1);
+        jl1.setBounds(0, 400, 690, 30);
+        jl1.setFont(new Font("", Font.BOLD, 24));
+        jl1.setForeground(Color.decode("#375a7f"));
+
+
+        jl.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JFrame win = new JFrame();
+                win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                win.setResizable(false);
+                win.setTitle("name_to_be_decidedd");
+                closeThis();
+
+                GamePanel gamepanel = new GamePanel(win);
+                win.add(gamepanel);
+                win.pack();
+
+                win.setLocationRelativeTo(null);
+                win.setVisible(true);
+
+                gamepanel.startGameThread();
+            }
+        });
+
+        jl1.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                closeThis();
+            }
+        });
+
+        jf.pack();
+        jf.setVisible(true);
+    }
+    public static void closeThis() {
+        jf.dispose();
+
+    }}
---
+++ Documents/src/main/java/MazeGame/SuccessMenu.java
@@ -0,0 +1,56 @@
+package MazeGame;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class SuccessMenu {
+    static JFrame jf = new JFrame("Success Menu");
+    public static void firtPage(int score) {
+
+        jf.setPreferredSize(new Dimension(700, 700));
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jf.setLayout(null);
+
+
+        JLabel jb=new JLabel("Congratulations! ! ! Escape successfully",JLabel.CENTER);
+        jb.setPreferredSize(new Dimension(680, 30));
+        jf.add(jb);
+        jb.setBounds(0, 200, 690, 30);
+        jb.setFont(new Font("", Font.BOLD, 24));
+        jb.setForeground(Color.decode("#375a7f"));
+        JLabel jb1=new JLabel("Your Score is: "+score,JLabel.CENTER);
+        jb1.setPreferredSize(new Dimension(680, 30));
+        jf.add(jb1);
+        jb1.setBounds(0, 300, 690, 30);
+        jb1.setFont(new Font("", Font.BOLD, 24));
+        jb1.setForeground(Color.decode("#375a7f"));
+
+        JButton jl1 = new JButton("EXIT");
+        jl1.setPreferredSize(new Dimension(680, 30));
+        jf.add(jl1);
+        jl1.setBounds(0, 400, 690, 30);
+        jl1.setFont(new Font("", Font.BOLD, 24));
+        jl1.setForeground(Color.decode("#375a7f"));
+
+
+
+        jl1.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.exit(0);
+            }
+        });
+
+        jf.pack();
+        jf.setVisible(true);
+    }
+    public static void closeThis() {
+        jf.dispose();
+
+    }}
