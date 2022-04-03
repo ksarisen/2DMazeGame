54879edc50ab2846a8090a646c602addcd53b1a6
Kerem Sarisen <kerem_sarisen@sfu.ca>
2022-04-03 12:03:45 -0700

    Created test classes for RewardsAndPunishments package

---
+++ Documents/src/test/java/RewardsAndPunishments/PunishmentGeneratorTest.java
@@ -0,0 +1,21 @@
+package RewardsAndPunishments;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PunishmentGeneratorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getPunishmentsList() {
+    }
+
+    @Test
+    void generatePunishment() {
+    }
+}
---
+++ Documents/src/test/java/RewardsAndPunishments/RewardGeneratorTest.java
@@ -0,0 +1,20 @@
+package RewardsAndPunishments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RewardGeneratorTest {
+
+    @Test
+    void getRewardsList() {
+    }
+
+    @Test
+    void generateRegularReward() {
+    }
+
+    @Test
+    void generateBonusReward() {
+    }
+}
