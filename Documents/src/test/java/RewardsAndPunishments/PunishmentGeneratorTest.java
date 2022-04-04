7e860e5729cd804647fc7020238ceef848e7fc50
Kerem Sarisen <kerem_sarisen@sfu.ca>
2022-04-03 17:53:11 -0700

    implemented test classes for generators

--- Documents/src/test/java/RewardsAndPunishments/PunishmentGeneratorTest.java
+++ Documents/src/test/java/RewardsAndPunishments/PunishmentGeneratorTest.java
@@ -1,21 +1,31 @@
 package RewardsAndPunishments;
 
+import MazeGame.GamePanel;
+import Textures.Image;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PunishmentGeneratorTest {
 
+    PunishmentGenerator punishmentGenerator;
+
+    JFrame frame = new JFrame();
+    Image punishmentImg = new Image("Construction.png");
+
     @BeforeEach
     void setUp() {
+        punishmentGenerator = new PunishmentGenerator(new GamePanel(frame));
     }
 
     @Test
-    void getPunishmentsList() {
-    }
+    void isPunishmentGenerated() {
 
-    @Test
-    void generatePunishment() {
+        assertEquals(punishmentGenerator.getPunishmentsList().size(), 4, "The amount of the punishments that are generated in the list is not what it should be which is 4.");
+        assertEquals(punishmentGenerator.getPunishmentsList().get(0).getValue(), 15, "The value of the punishment is not 15!" );
+        assertEquals(punishmentGenerator.getPunishmentsList().get(0).getTexture(), punishmentImg, "The assigned image of the punishment is wrong.");
     }
 }
--- Documents/src/test/java/RewardsAndPunishments/RewardGeneratorTest.java
+++ Documents/src/test/java/RewardsAndPunishments/RewardGeneratorTest.java
@@ -1,20 +1,40 @@
 package RewardsAndPunishments;
 
+import MazeGame.GamePanel;
+import Textures.Image;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RewardGeneratorTest {
 
-    @Test
-    void getRewardsList() {
+    RewardGenerator rewardGenerator;
+
+    JFrame frame = new JFrame();
+    Image regRewardImg = new Image("gas.png");
+    Image bonusRewardImg = new Image("money.png");
+
+    @BeforeEach
+    void setUp() {
+        rewardGenerator = new RewardGenerator(new GamePanel(frame));
     }
 
     @Test
-    void generateRegularReward() {
+    void isRegularRewardGenerated() {
+
+        assertEquals(rewardGenerator.getRewardsList().size(), 10, "The amount of the rewards that are generated in the list is not what it should be which is 10.");
+        assertEquals(rewardGenerator.getRewardsList().get(0).getValue(), 10, "The value of the regular reward is not 10!" );
+        assertEquals(rewardGenerator.getRewardsList().get(0).getTexture(), regRewardImg, "The assigned image of the generated regular reward is wrong.");
     }
 
     @Test
-    void generateBonusReward() {
+    void isBonusRewardGenerated() {
+
+        assertEquals(rewardGenerator.getRewardsList().size(), 10, "The amount of the rewards that are generated in the list is not what it should be which is 10.");
+        assertEquals(rewardGenerator.getRewardsList().get(rewardGenerator.getRewardsList().size()-1).getValue(), 25, "The value of the bonus reward is not 10!" );
+        assertEquals(rewardGenerator.getRewardsList().get(rewardGenerator.getRewardsList().size()-1).getTexture(), bonusRewardImg, "The assigned image of the generated bonus reward is wrong.");
     }
 }
