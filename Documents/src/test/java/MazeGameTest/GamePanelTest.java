d06c350bb97c171577c1c3c69ba01cc64ae77477
Reece Landry <reece_landry@sfu.ca>
2022-04-04 02:19:22 +0000

    Added game panel tests

---
+++ Documents/src/test/java/MazeGameTest/GamePanelTest.java
@@ -0,0 +1,35 @@
+package test.java.MazeGameTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.swing.JFrame;
+
+import org.junit.jupiter.api.*;
+
+import MazeGame.GamePanel;
+
+public class GamePanelTest {
+
+	GamePanel panel;
+	
+	@BeforeEach
+	void setUp() {
+		//create new player
+		JFrame win = new JFrame();
+		panel = new GamePanel(win);
+		
+	}
+	
+	@Test
+	void testBuildGame() {
+		//test player functions
+		panel.load(0);
+		assertNotEquals(panel.level, null, "Failed to create level");
+	}
+	
+	@Test
+	void testGameThread() {
+		panel.startGameThread();
+		assertNotNull(panel.gameThread);
+	}
+}
