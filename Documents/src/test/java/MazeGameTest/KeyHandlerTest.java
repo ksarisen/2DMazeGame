f001158b0fcc9c90d6cfc290ccf6c6decb277a1d
maintainer <yuwenj@sfu.ca>
2022-04-04 22:59:45 -0700

    test all method in the keyhandler class.

--- Documents/src/test/java/MazeGameTest/KeyHandlerTest.java
+++ Documents/src/test/java/MazeGameTest/KeyHandlerTest.java
@@ -18,12 +18,72 @@ public class KeyHandlerTest {
         k=new KeyHandler();
     }
     @Test
-    void HandlerTheKeyPressDown()
-
+    void HandlerTheKeyPressRight()
     {
         Button a = new Button("click");
         KeyEvent e=new KeyEvent(a, 1, 20, 1, 68, 'a');
         k.keyPressed(e);//"D" in key broad;
-        assertTrue(k.rightPressed);
+        assertTrue(k.rightPressed,"fail to read the press D command from key board");
+    }
+    @Test
+    void HandlerTheKeyPressUp()
+    {
+        Button a = new Button("click");
+        KeyEvent e=new KeyEvent(a, 1, 20, 1, 87, 'a');
+        k.keyPressed(e);//"W" in key broad;
+        assertTrue(k.upPressed,"fail to read the press W command from key board");
+    }
+    @Test
+    void HandlerTheKeyPressLeft()
+    {
+        Button a = new Button("click");
+        KeyEvent e=new KeyEvent(a, 1, 20, 1, 65, 'a');
+        k.keyPressed(e);//"A" in key broad;
+        assertTrue(k.leftPressed,"fail to read the press A command from key board");
+    }
+    @Test
+    void HandlerTheKeyPressDown()
+    {
+        Button a = new Button("click");
+        KeyEvent e=new KeyEvent(a, 1, 20, 1, 83, 'a');
+        k.keyPressed(e);//"S" in key broad;
+        assertTrue(k.downPressed,"fail to read the press S command from key board");
+    }
+
+    @Test
+    void HandlerTheKeyReleaseRight()
+    {
+        k.rightPressed=true;
+        Button a = new Button("click");
+        KeyEvent e=new KeyEvent(a, 1, 20, 1, 68, 'a');
+        k.keyReleased(e);//"D" in key broad;
+        assertFalse(k.rightPressed,"fail to read the release D command from key board");
+    }
+    @Test
+    void HandlerTheKeyReleaseUp()
+    {
+        k.upPressed=true;
+        Button a = new Button("click");
+        KeyEvent e=new KeyEvent(a, 1, 20, 1, 87, 'a');
+        k.keyReleased(e);//"W" in key broad;
+        assertFalse(k.upPressed,"fail to read the release W command from key board");
+    }
+    @Test
+    void HandlerTheKeyReleaseLeft()
+    {
+        k.leftPressed=true;
+        Button a = new Button("click");
+        KeyEvent e=new KeyEvent(a, 1, 20, 1, 65, 'a');
+        k.keyReleased(e);//"A" in key broad;
+        assertFalse(k.leftPressed,"fail to read the release A command from key board");
+    }
+    @Test
+    void HandlerTheKeyReleaseDown()
+    {
+        k.downPressed=true;
+        Button a = new Button("click");
+        KeyEvent e=new KeyEvent(a, 1, 20, 1, 83, 'a');
+        k.keyReleased(e);//"S" in key broad;
+        assertFalse(k.downPressed,"fail to read the release S command from key board");
     }
 }
